0b04621423aeedd16db40735f0c955b36b56284a
김진현 <jinhyeon0810@gmail.com>
2024-11-23 00:47:14 +0900

    add 동영상추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,15 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/qynlJNIb4SU?si=IdhHKr8iBWTpthlg"
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
